41d6629a8761fe1368c875eac65d8cc04923d411
AlexandrLytkin <Dracca@mail.ru>
2022-12-13 15:10:26 +0300

    first commit

---
+++ src/main/java/com/example/apprecipes/controllers/FirstControllers.java
@@ -0,0 +1,23 @@
+package com.example.apprecipes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstControllers {
+    @GetMapping
+    public String getApplicationLaunched() {
+        return "Приложение запущено!\n" +
+                "добавьте в конце адрессной строки /info для получения информации ";
+    }
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return "Лыткин Александр Александрович\n" +
+                "Проект Recipes\n" +
+                "Дата создания проекта 10.12.2022\n" +
+                "В проекте будут рецепты, для приготовления изысканных блюд!";
+
+    }
+    
+}
